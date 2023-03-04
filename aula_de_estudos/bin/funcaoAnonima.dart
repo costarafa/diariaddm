88261c6b8a476b2eb764078f1712ef95e21890e6
Rafaela Costa <39055398+costarafa@users.noreply.github.com>
2023-03-03 22:07:57 -0300

    Add files via upload

---
+++ aula_de_estudos/bin/funcaoAnonima.dart
@@ -0,0 +1,7 @@
+import 'package:aula_de_estudos/aula_de_estudos.dart' as aula_de_estudos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aula_de_estudos.calculate()}!');
+
+  //os parametros opcionais são usados quando
+}
---
+++ aula_de_estudos/test/aula_de_estudos_test.dart
@@ -0,0 +1,8 @@
+import 'package:aula_de_estudos/aula_de_estudos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
